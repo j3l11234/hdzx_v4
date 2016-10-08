4ad0c8f3a750c65735cec3f13f5e80756121acf1
j3l11234 <297259024@qq.com>
2016-10-08 16:11:06 +0800

    版权信息

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -82,9 +82,8 @@ AppAsset::register($this);
 
 <footer class="footer">
     <div class="container">
-        <p class="pull-left">&copy; My Company <?= date('Y') ?></p>
-
-        <p class="pull-right"><?= Yii::powered() ?></p>
+        <p class="pull-left">&copy; 北京交通大学 学生活动服务中心 <?= date('Y') ?></p>
+        <p class="pull-right">powered by <a href="http://blog.j3l11234.com">j3l11234</a></p>
     </div>
 </footer>
 <?php $this->endBody() ?>
