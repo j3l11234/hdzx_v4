0bf15a37e09e8b1f55d8bdd00e90ea3b7d820dfd
j3l11234 <297259024@qq.com>
2016-11-27 23:59:34 +0800

    7点验证码临时解决方案

--- frontend/actions/MyCaptchaAction.php
+++ frontend/actions/MyCaptchaAction.php
@@ -12,6 +12,57 @@ use yii\web\Response;
 class MyCaptchaAction extends CaptchaAction {
 
     /**
+     * Gets the verification code.
+     * @param bool $regenerate whether the verification code should be regenerated.
+     * @return string the verification code.
+     */
+    public function getVerifyCode($regenerate = false)
+    {
+        if ($this->fixedVerifyCode !== null) {
+            return $this->fixedVerifyCode;
+        }
+        $session = Yii::$app->getSession();
+        $session->open();
+        $name = $this->getSessionKey();
+        if ($session[$name] === null || $regenerate) {
+            $session[$name] = $this->generateVerifyCode();
+            $session[$name . 'count'] = 1;
+            $session[$name . 'time'] = time();
+        }
+        return $session[$name];
+    }
+
+    /**
+     * Validates the input to see if it matches the generated code.
+     * @param string $input user input
+     * @param bool $caseSensitive whether the comparison should be case-sensitive
+     * @return bool whether the input is valid
+     */
+    public function validate($input, $caseSensitive)
+    {
+        $code = $this->getVerifyCode();
+        $valid = $caseSensitive ? ($input === $code) : strcasecmp($input, $code) === 0;
+        $session = Yii::$app->getSession();
+        $session->open();
+
+        //临时解决方案
+        $timeout = false;
+        $name = $this->getSessionKey() . 'time';
+        if ($session[$name] < strtotime('07:00:00') && time() >= strtotime('07:00:00')) {
+            $timeout = true;
+            $valid = false;
+        }
+
+        $name = $this->getSessionKey() . 'count';
+        $session[$name] = $session[$name] + 1;
+        if ($valid || $session[$name] > $this->testLimit && $this->testLimit > 0 || $timeout) {
+            $this->getVerifyCode(true);
+        }
+        return $valid;
+    }
+
+
+    /**
      * @inheritdoc
      */
     public function run() {
